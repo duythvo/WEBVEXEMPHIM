fc5c5eb0c7236c7b261444cf67d1e5ffafce0f6d
Star-code1 <acestargmer@gmail.com>
2024-01-31 13:58:23 +0700

    tst
    uyy

---
+++ js/TinTuc.js
@@ -0,0 +1,13 @@
+
+function hidedienanh(){
+    var a = document.getElementById("dienanh");
+    var b = document.getElementById("tuyendung");
+    a.style.display='none';
+    b.style.display='block';
+}
+function hidetuyendung(){
+    var a = document.getElementById("dienanh");
+    var b = document.getElementById("tuyendung");    
+    a.style.display='block';
+    b.style.display='none';
+}
