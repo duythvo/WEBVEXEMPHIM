54993855b94aefd7b255ed372395a144e1c49f33
NDuy <nhatduy20072004@gmail.com>
2024-02-01 16:02:33 +0700

    a

--- js/Home.js
+++ js/Home.js
@@ -1,3 +1,15 @@
 function hideCurrentMovie(){
-    
+    var currentmovie = document.getElementById('dangchieu');
+    var upcoming = document.getElementById('sapchieu');
+
+    currentmovie.style.display='none';
+    upcoming.style.display='block';
+}
+
+function hideUpcomingMovie(){
+    var currentmovie = document.getElementById('dangchieu');
+    var upcoming = document.getElementById('sapchieu');
+
+    currentmovie.style.display='block';
+    upcoming.style.display='none';
 }
