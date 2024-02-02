ab50b8ad0913af103ab4a7b8ec7988fa7e9b3f4a
NDuy <nhatduy20072004@gmail.com>
2024-02-02 12:06:32 +0700

    a

--- js/Home.js
+++ js/Home.js
@@ -2,14 +2,35 @@ function hideCurrentMovie(){
     var currentmovie = document.getElementById('dangchieu');
     var upcoming = document.getElementById('sapchieu');
 
-    currentmovie.style.display='none';
-    upcoming.style.display='block';
+    currentmovie.style.display = 'none';
+    upcoming.style.display = 'block';
 }
 
 function hideUpcomingMovie(){
     var currentmovie = document.getElementById('dangchieu');
     var upcoming = document.getElementById('sapchieu');
 
-    currentmovie.style.display='block';
-    upcoming.style.display='none';
+    currentmovie.style.display = 'block';
+    upcoming.style.display = 'none';
 }
+
+
+window.addEventListener('scroll',function(){
+    var header = document.getElementById('header');
+    var headerhide = document.getElementById('headerhide');
+    if(this.window.scrollY > 0){
+        header.classList.remove('active');
+        header.classList.add('hidden');
+        headerhide.classList.add('active');
+        headerhide.classList.remove('hidden');
+
+    }else{
+        if(this.window.scrollY == 0){
+            header.classList.remove('hidden');
+            header.classList.add('active');
+            headerhide.classList.remove('active');
+            headerhide.classList.add('hidden');
+        }
+    }
+})
+
