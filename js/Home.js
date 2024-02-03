cd63b3f9552c3f48ed47c01077b23f382aa7c119
Star-code1 <acestargmer@gmail.com>
2024-02-03 17:07:32 +0700

    THUY 5:07

--- js/Home.js
+++ js/Home.js
@@ -1,50 +1,61 @@
-function hideCurrentMovie(){
-    var currentmovie = document.getElementById('dangchieu');
-    var upcoming = document.getElementById('sapchieu');
-
-    currentmovie.style.display = 'none';
-    upcoming.style.display = 'block';
-}
-
-function hideUpcomingMovie(){
-    var currentmovie = document.getElementById('dangchieu');
-    var upcoming = document.getElementById('sapchieu');
-
-    currentmovie.style.display = 'block';
-    upcoming.style.display = 'none';
-}
-
-
-window.addEventListener('scroll',function(){
-    var header = document.getElementById('header');
-    var headerhide = document.getElementById('headerhide');
-    if(this.window.scrollY > 0){
-        header.classList.remove('active');
-        header.classList.add('hidden');
-        headerhide.classList.add('active');
-        headerhide.classList.remove('hidden');
-
-    }else{
-        if(this.window.scrollY == 0){
-            header.classList.remove('hidden');
-            header.classList.add('active');
-            headerhide.classList.remove('active');
-            headerhide.classList.add('hidden');
-        }
+let selectedElement = null;
+function ClickColor (element) {
+    if (selectedElement !== null) {
+        selectedElement.classList.remove("clicked");
     }
-})
+    element.classList.add("clicked");
+    selectedElement = element;
+}
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 5,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+function hideCurrentMovie() {
 
-})
 
-window.addEventListener()
+  var currentmovie = document.getElementById("dangchieu");
+  var upcoming = document.getElementById("sapchieu");
+
+  currentmovie.style.display = "none";
+  upcoming.style.display = "block";
+}
+
+function hideUpcomingMovie() {
+
+
+  var currentmovie = document.getElementById("dangchieu");
+  var upcoming = document.getElementById("sapchieu");
+
+  currentmovie.style.display = "block";
+  upcoming.style.display = "none";
+}
+
+
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("header");
+  var headerhide = document.getElementById("headerhide");
+  if (this.window.scrollY > 0) {
+    header.classList.remove("active");
+    header.classList.add("hidden");
+    headerhide.classList.add("active");
+    headerhide.classList.remove("hidden");
+  } else {
+    if (this.window.scrollY == 0) {
+      header.classList.remove("hidden");
+      header.classList.add("active");
+      headerhide.classList.remove("active");
+      headerhide.classList.add("hidden");
+    }
+  }
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 5,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+window.addEventListener();
