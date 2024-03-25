39d39bb131a65fa28b978aaeec6d8d039448b75e
Star-code1 <acestargmer@gmail.com>
2024-03-25 23:20:13 +0700

    11:20

--- js/TinTuc.js
+++ js/TinTuc.js
@@ -77,33 +77,6 @@ buttonLogin.addEventListener("click", () => {
   }
 });
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  autoplay: {
-    delay: 4500,
-    disableOnInteraction: false,
-  },
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 5,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 5,
-  autoplay: {
-    delay: 2000,
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+
+
