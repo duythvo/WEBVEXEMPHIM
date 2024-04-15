2e14e1ee56495da0d78f95941cd9bcf00eb4a32f
Star-code1 <acestargmer@gmail.com>
2024-04-15 23:28:29 +0700

    12:28

--- js/DatVe.js
+++ js/DatVe.js
@@ -24,8 +24,8 @@ $(document).ready(function () {
     }
     cacGheChon.html("Ghế :" + listTenGhe.join(", "));
     tienCacGheChon.html(GiaGhe + "   đ");
-      let tong = GiaGhe + combo
-      $("#total").html( tong+"   đ")
+    let tong = GiaGhe + combo;
+    $("#total").html(tong + "   đ");
   });
   $(".ghedoi").click(function () {
     var cacGheChon = $("#tenghe");
@@ -52,110 +52,173 @@ $(document).ready(function () {
     }
     cacGheChon.html("Ghế :" + listTenGhe.join(", "));
     tienCacGheChon.html(GiaGhe + "   đ");
-          let tong = GiaGhe + combo
-      $("#total").html( tong+"   đ")
+    let tong = GiaGhe + combo;
+    $("#total").html(tong + "   đ");
   });
-  
-  $("#ngaychieu,#suatchieu").change(function (){
+
+  $("#ngaychieu,#suatchieu").change(function () {
     var ngaychieu = $("#ngaychieu").val();
     var suatchieu = $("#suatchieu").val();
-    $("#film_time").html("Ngày  " + ngaychieu+ " " + suatchieu +" - Rạp 01") 
-  }) ;
-  
+    $("#film_time").html("Ngày  " + ngaychieu + " " + suatchieu + " - Rạp 01");
+  });
+
   var pepngot = 0;
-  var upngot=0;
-  var Mirindangot = 0
-  var pepphomai = 0
-  var upphomai = 0
-  var Mirindaphomai = 0
-  $("#plusPN").click(function(){
-    pepngot = pepngot +1;
-    $("#pepngot").html(pepngot)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#minusPN").click(function(){
-    pepngot = pepngot -1;
-    if(pepngot<0) pepngot=0;
-    $("#pepngot").html(pepngot)
-        combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#plusPP").click(function(){
-    pepphomai = pepphomai +1;
-    $("#pepphomai").html(pepphomai)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#minusPP").click(function(){
-    pepphomai = pepphomai -1;
-    if(pepphomai<0) pepphomai=0;
-    $("#pepphomai").html(pepphomai)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#minus7N").click(function(){
-    upngot = upngot -1;
-    if(upngot<0) upngot=0;
-    $("#7upngot").html(upngot)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#plus7N").click(function(){
-    upngot = upngot +1;
-    $("#7upngot").html(upngot)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#plus7P").click(function(){
-    upphomai = upphomai +1;
-    $("#7upphomai").html(upphomai)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#minus7P").click(function(){
-    upphomai = upphomai -1;
-    if(upphomai<0) upphomai=0;
-    $("#7upphomai").html(upphomai)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#plusMN").click(function(){
-    Mirindangot = Mirindangot +1;
-    $("#mirindangot").html(Mirindangot)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#minusMN").click(function(){
-    Mirindangot = Mirindangot -1;
-    if(Mirindangot<0) Mirindangot=0;
-    $("#mirindangot").html(Mirindangot)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#plusMP").click(function(){
-    Mirindaphomai = Mirindaphomai +1;
-    $("#mirindaphomai").html(Mirindaphomai)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
-  })
-  $("#minusMP").click(function(){
-    Mirindaphomai = Mirindaphomai -1;
-    if(Mirindaphomai<0) Mirindaphomai=0;
-    $("#mirindaphomai").html(Mirindaphomai)
-    combo = (pepngot + upngot + Mirindangot)*67000 + (pepphomai + upphomai + Mirindaphomai)*75000
-    $("#tiencombo").html(combo+"  đ")
-    $("#total").html(GiaGhe + combo + "   đ")
+  var upngot = 0;
+  var Mirindangot = 0;
+  var pepphomai = 0;
+  var upphomai = 0;
+  var Mirindaphomai = 0;
+  $("#plusPN").click(function () {
+    pepngot = pepngot + 1;
+    $("#pepngot").html(pepngot);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#minusPN").click(function () {
+    pepngot = pepngot - 1;
+    if (pepngot < 0) pepngot = 0;
+    $("#pepngot").html(pepngot);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#plusPP").click(function () {
+    pepphomai = pepphomai + 1;
+    $("#pepphomai").html(pepphomai);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#minusPP").click(function () {
+    pepphomai = pepphomai - 1;
+    if (pepphomai < 0) pepphomai = 0;
+    $("#pepphomai").html(pepphomai);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#minus7N").click(function () {
+    upngot = upngot - 1;
+    if (upngot < 0) upngot = 0;
+    $("#7upngot").html(upngot);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#plus7N").click(function () {
+    upngot = upngot + 1;
+    $("#7upngot").html(upngot);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#plus7P").click(function () {
+    upphomai = upphomai + 1;
+    $("#7upphomai").html(upphomai);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#minus7P").click(function () {
+    upphomai = upphomai - 1;
+    if (upphomai < 0) upphomai = 0;
+    $("#7upphomai").html(upphomai);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#plusMN").click(function () {
+    Mirindangot = Mirindangot + 1;
+    $("#mirindangot").html(Mirindangot);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#minusMN").click(function () {
+    Mirindangot = Mirindangot - 1;
+    if (Mirindangot < 0) Mirindangot = 0;
+    $("#mirindangot").html(Mirindangot);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#plusMP").click(function () {
+    Mirindaphomai = Mirindaphomai + 1;
+    $("#mirindaphomai").html(Mirindaphomai);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+  $("#minusMP").click(function () {
+    Mirindaphomai = Mirindaphomai - 1;
+    if (Mirindaphomai < 0) Mirindaphomai = 0;
+    $("#mirindaphomai").html(Mirindaphomai);
+    combo =
+      (pepngot + upngot + Mirindangot) * 67000 +
+      (pepphomai + upphomai + Mirindaphomai) * 75000;
+    $("#tiencombo").html(combo + "  đ");
+    $("#total").html(GiaGhe + combo + "   đ");
+  });
+
+  $("#hoten").on("blur", function (e) {
+    var hoten = $("#hoten").val();
+    var checkhoten = /^[A-ZĐ][a-zđà-ỹ]*$/;
+    if (checkhoten.test(hoten) && hoten != "") {
+      $("#hoten").addClass("is-valid").removeClass("is-invalid");
+      $("#errorhoten").text("");
+    } else {
+      $("#hoten").addClass("is-invalid").removeClass("is-valid");
+      $("#errorhoten").text("Họ và tên bắt đầu bằng ký tự chữ hoa");
+    }
+  });
+  $("#email").on("blur", function (e) {
+    var email = $("#email").val();
+    var checkemail = /[A-Za-z0-9]+@gmail\.com$/;
+    if (checkemail.test(email) && email != "") {
+      $("#email").addClass("is-valid").removeClass("is-invalid");
+      $("#erroremail").text("");
+    } else {
+      $("#email").addClass("is-invalid").removeClass("is-valid");
+      $("#erroremail").text("Email phải đúng định dạng");
+    }
+  });
+  $("#sdt").on("blur", function (e) {
+    var sdt = $("#sdt").val();
+    var checksdt = /^0[0-9]{9}/;
+    if (checksdt.test(sdt) && sdt != "") {
+      $("#sdt").addClass("is-valid").removeClass("is-invalid");
+      $("#errorsdt").text("");
+    } else {
+      $("#sdt").addClass("is-invalid").removeClass("is-valid");
+      $("#errorsdt").text("Số điện thoại phải đúng định dạng");
+    }
+  });
+  $("#datve").click(function(){
+    if($("#errorhoten").hasClass("is-valid") && $("#erroremail").hasClass("is-valid") && $("#errorsdt").hasClass("is-valid") ){
+      
+    }
   })
 });
