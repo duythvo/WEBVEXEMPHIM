f4e6b02787396c01372c84b9b478bdc3b3a6c06c
Star-code1 <acestargmer@gmail.com>
2024-04-17 00:20:44 +0700

    12:20

--- js/Lichchieu.js
+++ js/Lichchieu.js
@@ -23,33 +23,18 @@ $(document).ready(function () {
   });
 });
 
-const cinemaData = {
-  hcm: {
-    "Rạp TH2D Gò Vấp": {
-      inf1: "Rạp TH2D Gò Vấp",
-      inf2: "116A Lê Lợi, Phường 7, Quận Gò Vấp, TP.Hồ Chí Minh",
-      inf3: "Đặt vé : 0914 941 896",
-    },
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
   },
-  gl: {
-    "Rạp TH2D Gia Lai": {
-      inf1: "Rạp TH2D Gia Lai",
-      inf2: "29A Lý Thường Kiệt, Thị trấn Đak Đoa, Huyện Đak Đoa, Tỉnh Gia Lai",
-      inf3: "Đặt vé : 0914 941 896",
-    },
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 5,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  dn: {
-    "Rạp TH2D Đồng Nai": {
-      inf1: "Rạp TH2D Đồng Nai",
-      inf2: "64 Lý Thái Tổ, Huyện Nhơn Trạch, Tỉnh Đồng Nai",
-      inf3: "Đặt vé : 0397 538 105",
-    },
-  },
-  dt: {
-    "Rạp TH2D Đồng Tháp": {
-      inf1: "Rạp TH2D Đồng Tháp",
-      inf2: "64 Lý Thái Tổ, Huyện Nhơn Trạch, Tỉnh Đồng Tháp",
-      inf3: "Đặt vé : 0397 538 104",
-    },
-  },
-};
+});
