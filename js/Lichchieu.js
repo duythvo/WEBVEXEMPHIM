d9a630770903b7252f4478b25fcbe04907f4d87b
Star-code1 <acestargmer@gmail.com>
2024-04-17 00:10:40 +0700

    12:10

--- js/Lichchieu.js
+++ js/Lichchieu.js
@@ -1,3 +1,28 @@
+$(document).ready(function () {
+  $("#chon").change(function () {
+    if ($(this).val()=="gv") {
+      $("#diaChi").html(
+        "<p>Rạp TH2D Gò Vấp</p><p>116A Lê Lợi, Phường 7, Quận Gò Vấp, TP.Hồ Chí Minh</p><p>Đặt vé : 0914 941 896</p>"
+      );
+    } 
+    if ($(this).val()=="gl") {
+      $("#diaChi").html(
+        "<p>Rạp TH2D Gia Lai</p><p>29A Lý Thường Kiệt, Thị trấn Đak Đoa, Huyện Đak Đoa, Tỉnh Gia Lai</p><p>Đặt vé : 0914 941 896</p>"
+      );
+    } 
+    if ($(this).val()=="dt") {
+      $("#diaChi").html(
+        "<p>Rạp TH2D Đồng Tháp</p><p>64 Lý Thái Tổ, Huyện Nhơn Trạch, Tỉnh Đồng Tháp</p><p>Đặt vé : 0397 538 105</p>"
+      );
+    } 
+    if ($(this).val()=="dn") {
+      $("#diaChi").html(
+        "<p>Rạp TH2D Đồng Nai</p><p>64 Lý Thái Tổ, Huyện Nhơn Trạch, Tỉnh Đồng Nai</p><p>Đặt vé : 0397 538 105</p>"
+      );
+    }
+  });
+});
+
 const cinemaData = {
   hcm: {
     "Rạp TH2D Gò Vấp": {
@@ -28,42 +53,3 @@ const cinemaData = {
     },
   },
 };
-
-function loadCinemas() {
-  var provinceSelect = document.getElementById("provinceSelect");
-  var cinemaSelect = document.getElementById("cinemaSelect");
-
-  cinemaSelect.innerHTML = "<option value=''>CHỌN RẠP</option>";
-
-  var selectedProvince = provinceSelect.value;
-
-  if (cinemaData[selectedProvince]) {
-    Object.keys(cinemaData[selectedProvince]).forEach(function (cinema) {
-      var option = document.createElement("option");
-      option.value = cinema;
-      option.text = cinema;
-      cinemaSelect.appendChild(option);
-    });
-  }
-
-  document.getElementById("cinemaInfo").textContent = "";
-}
-
-function displayCinemaInfo() {
-  var cinemaSelect = document.getElementById("cinemaSelect");
-  var cinemaInfoDiv = document.getElementById("cinemaInfo");
-
-  var selectedCinema = cinemaSelect.value;
-
-  var selectedProvince = document.getElementById("provinceSelect").value;
-
-  if (
-    cinemaData[selectedProvince] &&
-    cinemaData[selectedProvince][selectedCinema]
-  ) {
-    var cinemaInfo = cinemaData[selectedProvince][selectedCinema];
-    cinemaInfoDiv.innerHTML = `<p>${cinemaInfo.inf1}</p><p>${cinemaInfo.inf2}</p><p>${cinemaInfo.inf3}</p>`;
-  } else {
-    cinemaInfoDiv.textContent = "";
-  }
-}
--- js/TinTuc.js
+++ js/TinTuc.js
@@ -1,15 +1,3 @@
-function hidedienanh() {
-  var a = document.getElementById("dienanh");
-  var b = document.getElementById("tuyendung");
-  a.style.display = "none";
-  b.style.display = "block";
-}
-function hidetuyendung() {
-  var a = document.getElementById("dienanh");
-  var b = document.getElementById("tuyendung");
-  a.style.display = "block";
-  b.style.display = "none";
-}
 
 const buttonLogin = document.getElementById("button-login");
 const width = window.innerWidth;
