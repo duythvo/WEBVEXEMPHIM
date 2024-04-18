560e0e6ca0170c63236fd9b9da789948d43891a7
NDuy <nhatduy20072004@gmail.com>
2024-04-19 00:41:48 +0700

    Mission Completed 12:41pm 19/4/2024

---
+++ js/phim.js
@@ -0,0 +1,18 @@
+let selectedElement = null;
+function ClickColor(element) {
+  if (selectedElement !== null) {
+    selectedElement.classList.remove("clicked");
+  }
+  element.classList.add("clicked");
+  selectedElement = element;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var vanBan = document.querySelectorAll(".limitedText");
+
+  vanBan.forEach(function (vanBan) {
+    var mangTu = vanBan.innerText.split(/\s+/);
+    var gioiHanTu = mangTu.slice(0, 20).join(" "); //lấy 48 từ đầu tiên
+    vanBan.innerText = gioiHanTu;
+  });
+});
